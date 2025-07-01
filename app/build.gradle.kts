c2218bd65b251f980e0198c78df33b07e71071bb
mylinh <linhninh2@gmail.com>
2025-07-01 23:55:47 +0700

    add dependency exo player

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -94,4 +94,8 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+
+    // exo player
+    implementation(libs.androidx.media3.exoplayer)
+    implementation(libs.androidx.media3.ui)
 }
